499a830b2c70131b6c6afac7fd9eb151c2cb2534
nguyen thi thao <nguyentthaocv@gmail.com>
2024-10-02 21:01:35 +0700

    add comment

--- src/App.js
+++ src/App.js
@@ -8,11 +8,22 @@ function App() {
   return (
     <div>
  
-      {/* <CheckLogin/> */}
-      <ChatBots/>
+      <CheckLogin/>
+      {/* <ChatBots/> */}
       {/* <Dashboard/> */}
     </div>
   );
 }
 
 export default App;
+
+// {
+//   "content":"tôi muốn mua cuốn sách Biệt thự buồn số lượng 2 gửi về  Hà Nội Bắc Từ Liêm Nguyên Xá , địa chỉ email hhhhh@hmail.com số điện thoại 098143601" 
+//   }
+  //  "content":"cuốn sách sach tesst paymenty giá bao nhiêu?"
+  // "content":"cuốn sách sach tesst paymenty con ko so luong bao nhiau"
+  // "content":"tác giả sách sach tesst paymenty l"
+  //"content": 		"tôi muốn mua cuốn sách ABCXY số lượng 2 gửi về địa chỉ Hà Nội Bắc Từ Liêm Nguyên Xá , địa chỉ email hhhhh@hmail.com số điện thoại 098143601"
+  
+    //"content": 	"Loại sách văn học cổ điển rất hấp dẫn" 
+  
